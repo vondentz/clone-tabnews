c3f733363fd0ed7baad1d546175a98b0ea013f11
vondentz <contatojuniorvondentz@gmail.com>
2024-05-30 01:07:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Raquele eu amo você, se você me ama, dá uma risadinha! xD</h1>
+  return <h1>Raquele eu amo você, se você me ama, dá uma risadinha! xD</h1>;
 }
 
 export default Home;
